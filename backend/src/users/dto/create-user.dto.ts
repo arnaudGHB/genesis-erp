543f641bc3b9c818dcde0fcc08e63f4c58711b5d
arnaudGHB <128025532+arnaudGHB@users.noreply.github.com>
2025-10-23 17:11:21 +0100

    fix(backend2): implement robust build config for production

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -3,14 +3,14 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
+  name!: string; // <-- Ajouter le "!"
 
   @IsEmail()
   @IsNotEmpty()
-  email: string;
+  email!: string; // <-- Ajouter le "!"
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  password: string;
+  password!: string; // <-- Ajouter le "!"
 }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaModule } from 'src/prisma/prisma.module'; // <-- 1. IMPORTER
+import { PrismaModule } from '../prisma/prisma.module'; // <-- 1. IMPORTER
 
 @Module({
   imports: [PrismaModule], // <-- 2. AJOUTER ICI
