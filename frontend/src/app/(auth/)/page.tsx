6f9360b5bec86d1ca604f6da251d6c411ae20590
arnaudGHB <128025532+arnaudGHB@users.noreply.github.com>
2025-10-24 17:14:17 +0100

    feat(frontend): frontend stable qui se connecte à l'api de connexion dans render

---
+++ frontend/src/app/(auth/)/page.tsx
@@ -0,0 +1,10 @@
+export default function AuthLayout() {
+  return (
+    <div>
+      <h1>Auth Section</h1>
+      <nav>
+        <a href="/(auth)/login">Login</a>
+      </nav>
+    </div>
+  );
+}
